87a518ba0eef9a70243ad828c363400199b4c06c
AndreyKovalov <job4kav@gmail.com>
2024-12-31 15:46:16 +0100

    create copm Footer,Header,SideBar

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Home(): JSX.Element {
+  return <div className={styles.main}>1</div>;
+}
---
+++ components/shared/SideBar/SideBar.tsx
@@ -0,0 +1,5 @@
+import { SideBarProps } from "./SideBar.props";
+
+export const SideBar = ({ ...props }: SideBarProps): JSX.Element => {
+  return <div {...props}>SideBar</div>;
+};
